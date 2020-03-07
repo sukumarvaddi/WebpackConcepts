0603ffbd2a63de3a2dc163314a6ec636dbed4d9e
Sukumar Vaddi <SVaddi@paycor.com>
2020-03-07 10:48:57 -0600

    project setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import add from './add';
+import subtract from './subtract';
+
+console.log(add(4, 5));
+console.log(subtract(10, 5));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'development',
+
+    // Multiple Entry Points
+    /*     entry: {
+                          subtract: './src/subtract.js',
+                          add: './src/add.js'
+                      }
+                    */
+
+    // Single Entry point
+    /*  entry: './src/index.js' */
+
+    //multi point entry
+    entry: ['./src/index', './src/add', './src/subtract']
+};
