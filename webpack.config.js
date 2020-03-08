9860f1d1fbc8eefaaeecbe1487fda36c4967dde4
Sukumar Vaddi <SVaddi@paycor.com>
2020-03-08 08:46:24 -0500

    understanding output

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
+const path = require('path');
 module.exports = {
     mode: 'development',
 
+    /* 
     // Multiple Entry Points
     entry: {
         subtract: './src/subtract.js',
         add: './src/add.js'
-    }
-
-
+    },
+ */
     // Single Entry point
-    //  entry: './src/index.js'
+    // entry: './src/index.js',
 
-    //multi point entry
-    // entry: ['./src/index', './src/add', './src/subtract']
+    //multi main entry
+    // entry: ['./src/index', './src/add', './src/subtract'],
+
+    output: {
+        // filename: 'singleEntry.js'
+        /*   
+                    // Multi Entry Points
+                            filename: '[name].js', 
+                            path: path.resolve(__dirname, 'distribution')
+                        */
+        // filename:'multiMainEntry.js'
+    }
 };
